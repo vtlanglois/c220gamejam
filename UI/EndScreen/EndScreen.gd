ed0eb2efb70d38b323d4d6ddaca1b7a56afba05e
mmhamman <89564033+mmhamman@users.noreply.github.com>
2021-12-02 19:20:41 -0500

    hide button

--- UI/EndScreen/EndScreen.gd
+++ UI/EndScreen/EndScreen.gd
@@ -7,5 +7,6 @@ func _ready():
 	$Score.text = "Final Score: " + str(score) + "\nTime: " + str(Global.time) + "\nPeople Hit: " + str(Global.totalPeopleHit) +"\nWrenchs Collected: " + str(Global.totalWrenches)
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Game.tscn")
 	print("quit pressed")
+	get_tree().change_scene("res://Game.tscn")
+	
