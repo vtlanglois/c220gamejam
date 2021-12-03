036f2d8a7335ab02e880bfdc5ad8214763762c39
mmhamman <89564033+mmhamman@users.noreply.github.com>
2021-12-02 19:13:10 -0500

    hahahaha

--- UI/EndScreen/EndScreen.gd
+++ UI/EndScreen/EndScreen.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends Control
 
 
 
 func _ready():
 	var score = Global.score + (Global.totalWrenches*3) - int(Global.totalPeopleHit/2)
-	$Control/Score.text = "Final Score: " + str(score) + "\nTime: " + str(Global.time) + "\nPeople Hit: " + str(Global.totalPeopleHit) +"\nWrenchs Collected: " + str(Global.totalWrenches)
+	$Score.text = "Final Score: " + str(score) + "\nTime: " + str(Global.time) + "\nPeople Hit: " + str(Global.totalPeopleHit) +"\nWrenchs Collected: " + str(Global.totalWrenches)
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Game.tscn")
+	print("quit pressed")
